b46c0fc53094e1f56e3e2e36c61c5d6fac14e9da
Matt <mateo6379@gmail.com>
2019-07-16 12:42:02 -0500

    Clean up of CompositionService

--- Scribe/Compose/CompositionService.cs
+++ Scribe/Compose/CompositionService.cs
@@ -1,8 +1,9 @@
 ﻿using RattrapDev.Scribe.Compose.Model;
 using RattrapDev.Scribe.Term;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using Module = RattrapDev.Scribe.Compose.Model.Module;
 
 namespace RattrapDev.Scribe.Compose
 {
@@ -27,143 +28,30 @@ namespace RattrapDev.Scribe.Compose
                 if (attributes.Any(a => a is AggregateRoot))
                 {
                     var aggregateRootTerm = attributes.OfType<AggregateRoot>().First();
-                    if (!glossary.Modules.Any(m => m.Name == aggregateRootTerm.Name))
-                    {
-                        glossary.Modules.Add(new Module { Name = aggregateRootTerm.Module });
-                    }
-
-                    var module = glossary.Modules.First(m => m.Name == aggregateRootTerm.Module);
+                    var module = glossary.CreateOrGetModuleFrom(aggregateRootTerm);
                     var aggregateRootModel = new AggregateRootModel { Name = aggregateRootTerm.Name, Definition = aggregateRootTerm.Definition };
 
-                    foreach(var prop in t.GetProperties(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public))
-                    {
-                        var propAttributes = prop.PropertyType.GetCustomAttributes(true);
-                        if (propAttributes.Any(a => a is Entity))
-                        {
-                            var entityTerm = propAttributes.OfType<Entity>().First();
-                            aggregateRootModel.Entities.Add(entityTerm.Name);
-                        }
-                        else if (propAttributes.Any(a => a is ValueObject))
-                        {
-                            var term = propAttributes.OfType<ValueObject>().First();
-                            aggregateRootModel.ValueObjects.Add(term.Name);
-                        }
-                    }
-
-                    foreach(var method in t.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public))
-                    {
-                        var methodAttributes = method.GetCustomAttributes(true);
-                        if (methodAttributes.Any(a => a is CommandMethod))
-                        {
-                            var commandMethodTerm = methodAttributes.OfType<CommandMethod>().First();
-                            var domainEventTerms = commandMethodTerm.IssuedEvents == null ? Enumerable.Empty<DomainEvent>() : commandMethodTerm.IssuedEvents.Select(e => e.GetCustomAttributes().OfType<DomainEvent>().First()).ToList();
-
-                            var commandMethodModel = new CommandMethodModel
-                            {
-                                Name = commandMethodTerm.Name,
-                                Purpose = commandMethodTerm.Purpose,
-                                Events = domainEventTerms.Select(e => e.Name).ToList(),
-                            };
-
-                            aggregateRootModel.CommandMethods.Add(commandMethodModel);
-                        }
-                    }
+                    aggregateRootModel.Entities.AddRange(GetEntitiesFromProperties(t));
+                    aggregateRootModel.ValueObjects.AddRange(GetValueObjectsFromProperties(t));
+                    aggregateRootModel.CommandMethods.AddRange(GetCommandMethods(t));
 
                     module.Aggregates.Add(aggregateRootModel);
                 }
                 else if (attributes.Any(a => a is Entity))
                 {
                     var entityTerm = attributes.OfType<Entity>().First();
-                    if (!glossary.Modules.Any(m => m.Name == entityTerm.Name))
-                    {
-                        glossary.Modules.Add(new Module { Name = entityTerm.Module });
-                    }
-
-                    var module = glossary.Modules.First(m => m.Name == entityTerm.Module);
+                    var module = glossary.CreateOrGetModuleFrom(entityTerm);
                     var entityModel = new EntityModel { Name = entityTerm.Name, Definition = entityTerm.Definition };
 
-                    foreach (var prop in t.GetProperties(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public))
-                    {
-                        var propAttributes = prop.PropertyType.GetCustomAttributes(true);
-                        if (propAttributes.Any(a => a is ValueObject))
-                        {
-                            var term = propAttributes.OfType<ValueObject>().First();
-                            entityModel.ValueObjects.Add(term.Name);
-                        }
-                    }
-
-                    foreach (var method in t.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public))
-                    {
-                        var methodAttributes = method.GetCustomAttributes(true);
-                        if (methodAttributes.Any(a => a is CommandMethod))
-                        {
-                            var commandMethodTerm = methodAttributes.OfType<CommandMethod>().First();
-                            var domainEventTerms = commandMethodTerm.IssuedEvents == null ? Enumerable.Empty<DomainEvent>() : commandMethodTerm.IssuedEvents.Select(e => e.GetCustomAttributes().OfType<DomainEvent>().First()).ToList();
-
-                            var commandMethodModel = new CommandMethodModel
-                            {
-                                Name = commandMethodTerm.Name,
-                                Purpose = commandMethodTerm.Purpose,
-                                Events = domainEventTerms.Select(e => e.Name).ToList(),
-                            };
-
-                            entityModel.CommandMethods.Add(commandMethodModel);
-                        }
-                    }
-
-                    module.Entities.Add(entityModel);
-                }
-                else if (attributes.Any(a => a is Entity))
-                {
-                    var entityTerm = attributes.OfType<Entity>().First();
-                    if (!glossary.Modules.Any(m => m.Name == entityTerm.Name))
-                    {
-                        glossary.Modules.Add(new Module { Name = entityTerm.Module });
-                    }
-
-                    var module = glossary.Modules.First(m => m.Name == entityTerm.Module);
-                    var entityModel = new EntityModel { Name = entityTerm.Name, Definition = entityTerm.Definition };
-
-                    foreach (var prop in t.GetProperties(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public))
-                    {
-                        var propAttributes = prop.PropertyType.GetCustomAttributes(true);
-                        if (propAttributes.Any(a => a is ValueObject))
-                        {
-                            var term = propAttributes.OfType<ValueObject>().First();
-                            entityModel.ValueObjects.Add(term.Name);
-                        }
-                    }
-
-                    foreach (var method in t.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public))
-                    {
-                        var methodAttributes = method.GetCustomAttributes(true);
-                        if (methodAttributes.Any(a => a is CommandMethod))
-                        {
-                            var commandMethodTerm = methodAttributes.OfType<CommandMethod>().First();
-                            var domainEventTerms = commandMethodTerm.IssuedEvents == null ? Enumerable.Empty<DomainEvent>() : commandMethodTerm.IssuedEvents.Select(e => e.GetCustomAttributes().OfType<DomainEvent>().First()).ToList();
-
-                            var commandMethodModel = new CommandMethodModel
-                            {
-                                Name = commandMethodTerm.Name,
-                                Purpose = commandMethodTerm.Purpose,
-                                Events = domainEventTerms.Select(e => e.Name).ToList(),
-                            };
-
-                            entityModel.CommandMethods.Add(commandMethodModel);
-                        }
-                    }
+                    entityModel.ValueObjects.AddRange(GetValueObjectsFromProperties(t));
+                    entityModel.CommandMethods.AddRange(GetCommandMethods(t));
 
                     module.Entities.Add(entityModel);
                 }
                 else if (attributes.Any(a => a is ValueObject))
                 {
                     var valueObjectTerm = attributes.OfType<ValueObject>().First();
-                    if (!glossary.Modules.Any(m => m.Name == valueObjectTerm.Name))
-                    {
-                        glossary.Modules.Add(new Module { Name = valueObjectTerm.Module });
-                    }
-
-                    var module = glossary.Modules.First(m => m.Name == valueObjectTerm.Module);
+                    var module = glossary.CreateOrGetModuleFrom(valueObjectTerm);
                     var valueObjectModel = new ValueObjectModel { Name = valueObjectTerm.Name, Definition = valueObjectTerm.Definition };
 
                     module.ValueObjects.Add(valueObjectModel);
@@ -171,12 +59,7 @@ namespace RattrapDev.Scribe.Compose
                 else if (attributes.Any(a => a is DomainService))
                 {
                     var domainServiceTerm = attributes.OfType<DomainService>().First();
-                    if (!glossary.Modules.Any(m => m.Name == domainServiceTerm.Name))
-                    {
-                        glossary.Modules.Add(new Module { Name = domainServiceTerm.Module });
-                    }
-
-                    var module = glossary.Modules.First(m => m.Name == domainServiceTerm.Module);
+                    var module = glossary.CreateOrGetModuleFrom(domainServiceTerm);
                     var domainServiceModel = new DomainServiceModel { Name = domainServiceTerm.Name, Definition = domainServiceTerm.Definition };
 
                     module.DomainServices.Add(domainServiceModel);
@@ -184,12 +67,7 @@ namespace RattrapDev.Scribe.Compose
                 else if (attributes.Any(a => a is DomainEvent))
                 {
                     var domainEventTerm = attributes.OfType<DomainEvent>().First();
-                    if (!glossary.Modules.Any(m => m.Name == domainEventTerm.Name))
-                    {
-                        glossary.Modules.Add(new Module { Name = domainEventTerm.Module });
-                    }
-
-                    var module = glossary.Modules.First(m => m.Name == domainEventTerm.Module);
+                    var module = glossary.CreateOrGetModuleFrom(domainEventTerm);
                     var domainEventModel = new DomainEventModel { Name = domainEventTerm.Name, Definition = domainEventTerm.Definition };
 
                     module.DomainEvents.Add(domainEventModel);
@@ -197,6 +75,55 @@ namespace RattrapDev.Scribe.Compose
             }
 
             return glossary;
+        }
+
+        private List<string> GetValueObjectsFromProperties(Type t)
+        {
+            var valueObjects = new List<string>();
+            foreach (var valueObject in t.GetProperties(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public)
+                .Select(p => p.PropertyType.GetCustomAttributes().OfType<ValueObject>()).SelectMany(c => c))
+            {
+                valueObjects.Add(valueObject.Name);
+            }
+
+            return valueObjects;
+        }
+
+        private List<string> GetEntitiesFromProperties(Type t)
+        {
+            var entities = new List<string>();
+            foreach (var entity in t.GetProperties(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public)
+                .Select(p => p.PropertyType.GetCustomAttributes().OfType<Entity>()).SelectMany(c => c))
+            {
+                entities.Add(entity.Name);
+            }
+
+            return entities;
+        }
+
+        private List<CommandMethodModel> GetCommandMethods(Type t)
+        {
+            var commandMethodModels = new List<CommandMethodModel>();
+            foreach (var method in t.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public))
+            {
+                var methodAttributes = method.GetCustomAttributes(true);
+                if (methodAttributes.Any(a => a is CommandMethod))
+                {
+                    var commandMethodTerm = methodAttributes.OfType<CommandMethod>().First();
+                    var domainEventTerms = commandMethodTerm.IssuedEvents == null ? Enumerable.Empty<DomainEvent>() : commandMethodTerm.IssuedEvents.Select(e => e.GetCustomAttributes().OfType<DomainEvent>().First()).ToList();
+
+                    var commandMethodModel = new CommandMethodModel
+                    {
+                        Name = commandMethodTerm.Name,
+                        Purpose = commandMethodTerm.Purpose,
+                        Events = domainEventTerms.Select(e => e.Name).ToList(),
+                    };
+
+                    commandMethodModels.Add(commandMethodModel);
+                }
+            }
+
+            return commandMethodModels;
         }
     }
 }
--- Scribe/Compose/Model/Glossary.cs
+++ Scribe/Compose/Model/Glossary.cs
@@ -1,9 +1,21 @@
-﻿using System.Collections.Generic;
+﻿using RattrapDev.Scribe.Term;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace RattrapDev.Scribe.Compose.Model
 {
     public class Glossary
     {
         public List<Module> Modules { get; set; } = new List<Module>();
+
+        public Module CreateOrGetModuleFrom(BaseDomainAttribute term)
+        {
+            if (!Modules.Any(m => m.Name == term.Name))
+            {
+                Modules.Add(new Module { Name = term.Module });
+            }
+
+            return Modules.First(m => m.Name == term.Module);
+        }
     }
 }
